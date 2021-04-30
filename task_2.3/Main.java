a0cd1ec1c05fbeb386897977cc43a7df20f8fb95
OldZippo <83456256+OldZippo@users.noreply.github.com>
2021-04-30 20:34:58 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+	    String s="8";
+        for(int i=0; i<10; i++) {
+            System.out.println(s);
+            s=s+"8";
+        }
+    }
+}
