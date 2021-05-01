8b5e84dc31cdd9240912410dd994c8f00d408982
OldZippo <83456256+OldZippo@users.noreply.github.com>
2021-05-01 09:32:17 +0300

    Create Main.java

---
+++ task_2.5/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int iNumber;
+        iNumber = scan.nextInt();
+        if (iNumber == 10) System.out.println("Верно");
+        else System.out.println("Неверно");
+    }
+}
