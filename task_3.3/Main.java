3a51622549cf93f8173a810f7747d9b804b3821d
OldZippo <83456256+OldZippo@users.noreply.github.com>
2021-05-02 14:49:36 +0300

    Update Main.java

--- task_3.3/Main.java
+++ task_3.3/Main.java
@@ -1,11 +1,12 @@
 public class Main {
 
-   public static String IsEqually(int a, int b) {
-        if (a == b) return "true";
-        else return "false";
-   }
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+
         System.out.println(IsEqually(-20, -10));
         System.out.println(IsEqually(-20, -20));
     }
+        static String IsEqually(int a, int b) {
+            if (a == b) return "true";
+            else return "false";
+   }
 }
