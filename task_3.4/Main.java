5ce0073da397c23a0e31e626f2a2f591aeb5f709
OldZippo <83456256+OldZippo@users.noreply.github.com>
2021-05-02 15:08:36 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(Summ(1, 9));
+        System.out.println(Summ(4, 7));
+    }
+    static String Summ(int a, int b) {
+        if ((a + b)>10) return "true";
+        else return "false";
+    }
+}
